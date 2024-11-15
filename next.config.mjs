99a0813efbe1253e0ee35802f90bd38ac604972f
杨明俊 <35731372+ymj1078524201@users.noreply.github.com>
2024-11-15 18:32:18 +0800

    update

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+import "./env.mjs"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push("pino-pretty", "lokijs", "encoding")
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: /icon/,
+      use: ["@svgr/webpack"],
+    })
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      resourceQuery: { not: [/icon/] },
+      loader: "next-image-loader",
+      options: { assetPrefix: "" },
+    })
+    return config
+  },
+}
+
+export default nextConfig
